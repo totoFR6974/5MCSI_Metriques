78258a70b5c90a9e6e947dbe2f4bc5dd062f79cd
toto_ <92277567+totoFR6974@users.noreply.github.com>
2026-01-05 15:52:10 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -29,3 +29,8 @@ def meteo():
         temp_day_value = list_element.get('main', {}).get('temp') - 273.15 # Conversion de Kelvin en °c 
         results.append({'Jour': dt_value, 'temp': temp_day_value})
     return jsonify(results=results)
+
+
+@app.route("/rapport/")
+def mongraphique():
+    return render_template("graphique.html")
